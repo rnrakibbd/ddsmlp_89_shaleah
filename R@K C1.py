78f577676130c797e2d14dcd23a79e89f99b815c
rnrakibbd <azmshaleah@gmail.com>
2025-05-29 12:08:14 +0600

    first commit

---
+++ R@K C1.py
@@ -0,0 +1,23 @@
+# Multiplication table (from 1 to 10) in Python
+
+num = 12
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
+
+name = input("Name: ")
+age = input("Age: ")
+date_of_birth = input("Date of Birth: ")
+address = input("Address: ")
+nationality = input("Nationality: ")
+
+
+print("Name:", name)
+print("Age:", age)
+print("Date of Birth:", date_of_birth)
+print("Address:", address)
+print("Nationality:", nationality)
